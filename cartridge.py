577b679ed1947b5e10a33f5ee63d58f664b7519a
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-27 21:02:10 +0200

    feat: implement cli -> dump

---
+++ cartridge.py
@@ -0,0 +1,24 @@
+import click
+
+@click.group()
+def cli():
+    pass
+
+@click.group()
+def hdr():
+    pass
+
+@click.command()
+@click.argument("GBA_FILES", required=False, nargs=-1)
+def dump(gba_files):
+    for file in gba_files:
+        click.echo(file)
+    
+cli.add_command(hdr)
+hdr.add_command(dump)
+
+def main():
+    cli()
+
+if __name__ == "__main__":
+    main()
