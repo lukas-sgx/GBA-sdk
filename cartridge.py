34d568aa7b80189da39c045b29a2c158279b2d4b
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-05 22:22:24 +0200

    feat: add help message & description

--- cartridge.py
+++ cartridge.py
@@ -2,29 +2,36 @@ import click
 import os
 from header import Header
 
-@click.group()
+@click.group(help="Cartridge SDK CLI entry")
 def cli():
     pass
 
-@click.group()
+@click.group(help="Cartridge header interface")
 def hdr():
     pass
 
-@click.command()
+@click.command(help="dump / display cartridge header information")
 @click.argument("GBA_FILES", required=False, nargs=-1)
 def dump(gba_files):
     for file in gba_files:
         header = Header(file)
         header.display_header()
 
-@click.command()
+@click.command(help="build / build cartridge ROM")
 def build():
     command = ["make", "-C", "build"]
     os.execvp(command[0], command)
+
+@click.command(help="gen / generate cartridge ROM")
+@click.option("-b", "--binary", required=True, nargs=1, type=click.File('rb'), help="input binary file")
+@click.option("-o", "--output", required=True, nargs=1, type=click.File('wb'), help="output filename")
+def gen():
+    pass
     
 cli.add_command(hdr)
-cli.add_command(build)
+hdr.add_command(build)
 hdr.add_command(dump)
+hdr.add_command(gen)
 
 def main():
     cli()
