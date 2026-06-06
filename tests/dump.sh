845df98ceb3812ddb5ae2ccf8562efea566cdbe7
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-06 17:27:55 +0200

    feat(tests): implement dump ROM test

--- tests/dump.sh
+++ tests/dump.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
 
-cartridge hdr dump tests/Super_Mario_Advance.gba
+cartridge hdr dump tests/Super_Mario_Advance.gba > tests/result/dump-rom
+
+diff tests/expected/dump tests/result/dump-rom
--- tests/tester.sh
+++ tests/tester.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+mkdir -p tests/result
 bash ./tests/dump.sh
