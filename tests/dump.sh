0ff2fd0ae202c1a864f7d3783588fef02dcfc625
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-06 17:10:13 +0200

    fix(tests): update path of ROM

--- tests/dump.sh
+++ tests/dump.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-curl -L https://static.romsgames.net/a202606068c0w40DWa82/output.bin?mediaId=13610&attach=Super%20Mario%20Advance%202%3A%20Super%20Mario%20World.zip -o game.zip
-unzip game.zip
-ls 'Super Mario Advance 2 - Super Mario World (USA, Australia).gba'
+
 cartridge hrd dump 'Super Mario Advance 2 - Super Mario World (USA, Australia).gba'
