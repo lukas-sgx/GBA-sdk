5b03748c3d3106de7d01b7bec1b184e382f03e0f
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-06 15:02:19 +0200

    fix: cleanup devenv

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,8 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
   env.GREET = "devenv";
 
-  # nixpkgs.config.allowUnfree = true;
-  
-  # https://devenv.sh/packages/
   packages = [
     pkgs.libxkbcommon
     pkgs.vulkan-loader
@@ -19,19 +15,6 @@
     venv.enable = true;
   };
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  # 
-
-  # https://devenv.sh/basics/
   enterShell = ''
     pip install -e "." --quiet
     git --version # Use packages
@@ -43,20 +26,8 @@
     pkgs.libxkbcommon
   ];
 
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
