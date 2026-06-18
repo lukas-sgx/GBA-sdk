98e9b26ec6f0886ccc24311d6f741c792b4ce359
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-18 12:55:44 +0200

    feat: switch python call build -> cmake

--- cartridge/modules/build.py
+++ cartridge/modules/build.py
@@ -1,53 +1,39 @@
 import click
 import subprocess
 import os
+import glob
+from pathlib import Path
 from cartridge.services.generation import Generation
 
-@click.command(help="craft cartridge ROM (GBA)")
-@click.option("-s", "--source", required=True, type=click.STRING, multiple=True, help="input source(s) file(s)")
-@click.option("-o", "--output", required=True, nargs=1, type=click.File('wb'), help="output filename")
-def build(source: tuple, output: click.File):
-    name = output.name.split(".")[0]
+def listBin() -> list[str]:
+    return glob.glob("bin/*.bin")
 
+@click.command(help="craft cartridge ROM (GBA)")
+@click.option("-s", "--source", required=True, type=click.Path(exists=True), help="set CMake dir")
+def build(source: str) -> None:
     os.makedirs("bin", exist_ok=True)
     os.makedirs("build", exist_ok=True)
 
-    for file in source:
-            ext = file.split(".")[-1]
-            if ext == "c":
-                label = "CC] "
-            elif ext == "S":
-                label = "ASM]"
-            else:
-                click.echo(click.style(f"[ERR] unsupported file: {file}", fg="red"), err=True)
-                exit(1)
+    PREV_DIR = os.getcwd()
     
-            click.echo(
-                click.style(f"[{label} ", fg="blue", bold=True) +
-                click.style(file, bold=True) +
-                click.style(f" -> build/{name}.elf", fg="cyan")
-            )
+    os.chdir(source)
 
     subprocess.run([
-        "arm-none-eabi-gcc",
-        "-mcpu=arm7tdmi", "-mlittle-endian",
-        "-nostdlib",
-        "-o", f"build/{name}.elf",
-        *source,
-        "-T", "./build/linker/gba.ld"
+        "cmake",
+        "."
     ], check=True)
 
-    click.echo(
-        click.style("[BIN] ", fg="blue", bold=True) +
-        click.style(f"build/{name}.elf " , bold=True) +
-        click.style(f"-> bin/{name}.bin", fg="cyan")
-    )
+    os.chdir(PREV_DIR)
+
     subprocess.run([
-        "arm-none-eabi-objcopy",
-        "-O", "binary",
-        f"build/{name}.elf",
-        f"./bin/{name}.bin"
+        "cmake",
+        "--build",
+        source
     ], check=True)
 
-    Generation(f"./bin/{name}.bin", output, "filename", "BXXE")
+    for binary in listBin():
+        project = binary.split(".")[0]
+        output_path = f"{project}.gba"
         
+        with open(output_path, "wb") as output_file:
+            Generation(binary, output_file, project.split("/")[1], "BXXE")
