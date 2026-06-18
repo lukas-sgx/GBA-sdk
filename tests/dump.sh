2263dac759a8a273b8093c00b4cbe9e73793298a
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-18 13:18:22 +0200

    fix(tests): update GBA ROM tests name

--- tests/dump.sh
+++ tests/dump.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-cartridge hdr dump mini.gba > tests/result/dump-rom
+cartridge hdr dump ExampleGBA.gba > tests/result/dump-rom
 
 diff tests/expected/dump tests/result/dump-rom
