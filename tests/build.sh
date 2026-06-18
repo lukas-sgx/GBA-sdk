7d9fac129087bd45fb02230bcb699d5b80871188
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-18 13:22:48 +0200

    fix(tests): update path of GBA

--- tests/build.sh
+++ tests/build.sh
@@ -3,6 +3,6 @@
 set -e
 
 cartridge build -s build/example/
-cartridge hdr dump ExampleGBA.gba > tests/result/dump-build
+cartridge hdr dump bin/ExampleGBA.gba > tests/result/dump-build
 
 diff tests/result/dump-build tests/expected/build
