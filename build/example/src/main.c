0554d0e97737d589b7372d5c91945a895eae7058
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-18 22:34:31 +0200

    feat: setup multiple colors

--- build/example/src/main.c
+++ build/example/src/main.c
@@ -6,5 +6,9 @@ extern void dclear(uint16_t color);
 extern void main(void)
 {
     dinit();
-    dclear(31);
+    while (1) {
+        dclear(31);
+        dclear(28);
+        dclear(20);
+    }
 }
