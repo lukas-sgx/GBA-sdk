98b651b044befd3dc6b78dacf20cd28c498a6ee7
lukas-sgx <68616614+lukas-sgx@users.noreply.github.com>
2026-06-23 23:52:58 +0200

    feat: add font structure handle

---
+++ build/example/include/font.h
@@ -0,0 +1,20 @@
+#ifndef FONT_H
+    #define FONT_H
+
+    #include <stddef.h>
+    #include <stdint.h>
+
+typedef struct glyph_s {
+    size_t width;
+    size_t height;
+} glyph_t;
+
+typedef struct gba_font_s {
+    uint8_t type;
+    const glyph_t glyphs[94];
+    const uint8_t *bitmap;
+} gba_font_t;
+
+void init_font(gba_font_t *font, uint8_t type);
+
+#endif
