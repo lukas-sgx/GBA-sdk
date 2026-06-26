7050f60c3b3e8375ae521193fc7be753aea2454c
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-26 20:07:03 +0200

    feat: add info on init project

--- cartridge/modules/init.py
+++ cartridge/modules/init.py
@@ -10,11 +10,17 @@ def init():
     headers = {"User-Agent": "Python-Cartridge"}
     req = urllib.request.Request(url, headers=headers)
 
+    click.echo(
+        click.style("Downloading ", fg="green", bold=True) +
+        click.style(f"GBA-sdk v{__version__}\n") +
+        click.style(f"\t    {url}", fg=(138,138,138))
+    )
+
     with urllib.request.urlopen(req) as response:
         tar_data = io.BytesIO(response.read())
 
+        extracted_count = 0
         with tarfile.open(fileobj=tar_data, mode="r:gz") as tar:
-            extracted_count = 0
 
             for member in tar.getmembers():
                 parts = member.name.split("/", 1)
@@ -22,7 +28,15 @@ def init():
                     continue
 
                 relative_path = parts[1]
-                if relative_path.startswith(("sandbox/")) and not member.isdir():
+                if relative_path.startswith(("sandbox/", "libs/")) and not member.isdir():
                     member.name = relative_path
+                    click.echo(
+                        click.style("Extracting  ", fg="green", bold=True) +
+                        click.style(f"{member.name} -> .")
+                    )
                     tar.extract(member, path=".")
                     extracted_count += 1
+        click.echo(
+            click.style("Finished", fg="green", bold=True) +
+            click.style(f"    {extracted_count} dir(s) extracted\n")
+        )
