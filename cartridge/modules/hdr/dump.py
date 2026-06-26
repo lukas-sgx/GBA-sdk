3411fd428fd80bd6e7ea0a78577cb982a0f9937d
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-26 16:01:12 +0200

    style: change all helper description

--- cartridge/modules/hdr/dump.py
+++ cartridge/modules/hdr/dump.py
@@ -1,7 +1,7 @@
 import click
 from cartridge.services.header import Header
 
-@click.command(help="dump / display cartridge header information")
+@click.command(help="Dump cartridge header information")
 @click.argument("GBA_FILES", required=False, nargs=-1)
 def dump(gba_files):
     for file in gba_files:
