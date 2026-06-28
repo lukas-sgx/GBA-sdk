aed61480fa4bea36fdd3c138f35d984f9b4b7ff9
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-29 01:40:56 +0200

    feat: set proportional by default

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -6,10 +6,10 @@ extern void main(void)
     gba_font_t font;
 
     dinit();
-    init_font(&font, MONOSPACED);
+    init_font(&font, PROPORTIONAL);
     wait_vblank();
     dclear(0xDE7B);
-    dtext_opt(&font, 240 / 2, 160 / 2, 31, BG_NONE, DTEXT_HALIGN_MIDDLE, DTEXT_VALIGN_CENTER, "Hello World !");
+    dtext_opt(&font, 240 / 2, 160 / 2, 0x7FFF, BG_NONE, DTEXT_HALIGN_MIDDLE, DTEXT_VALIGN_CENTER, "Hello World !");
     while (1)
         wait_vblank();
 }
