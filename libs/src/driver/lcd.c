980b132aacd432ca168ae6a160af170431971000
lukas-sgx <lukas.soigneux@epitech.eu>
2026-07-25 16:56:58 +0200

    feat: implement declarative call for all drivers

--- libs/src/driver/lcd.c
+++ libs/src/driver/lcd.c
@@ -1,8 +1,8 @@
 #include "lcd.h"
+#include "driver.h"
 #include <stdint.h>
 
-void gba_lcd_driver_init(void)
-{
+void gba_lcd_driver_init(void) {
     GBA_LCD.DISPCNT.BG2 = 1;
     GBA_LCD.DISPCNT.BG_MODE = 3;
 }
@@ -17,8 +17,8 @@ void gba_lcd_vram_dpixel(uint16_t color, uint32_t x, uint32_t y)
 void gba_lcd_vram_clear(uint16_t color)
 {
     uint32_t color32 = color << 16 | color;
-    volatile uint32_t* vram32 = (volatile uint32_t*)GBA_LCD_VRAM;
-    
+    volatile uint32_t *vram32 = (volatile uint32_t *)GBA_LCD_VRAM;
+
     for (int32_t i = 0; i < 240 * 160 / 2; i++)
         (vram32)[i] = color32;
 }
@@ -28,3 +28,7 @@ void gba_lcd_wait_vblank(void)
     while (GBA_LCD.DISPSTAT.VBF & 1);
     while (!(GBA_LCD.DISPSTAT.VBF & 1));
 }
+
+WIRED_DECLARE_DRIVER(01, gba_lcd, .name = "LCD",
+    .configure = &gba_lcd_driver_init,
+);
--- libs/src/kernel/start.c
+++ libs/src/kernel/start.c
@@ -1,11 +1,16 @@
-#include "lcd.h"
+#include "driver.h"
+#include <stddef.h>
 
 extern void main();
 
 __attribute__((section(".text.kernel_init")))
 void kernel_init(void)
 {
-    gba_lcd_driver_init();
+    for (size_t i = 0; i < wired_driver_count(); i++) {
+        wired_drv_t *driver = &__wired_drivers[i];
+        if (driver->configure)
+            driver->configure();
+    }
     main();
     while (1);
 }
